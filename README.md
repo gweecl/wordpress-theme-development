157d30465b325dd34b881f7addd45db038567568
zeroiszero <>
2019-07-06 19:45:21 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# wordpress-theme-development
+
+## Timber Starter Theme
+
+* To use [Starter Theme](https://github.com/timber/starter-theme). Install and replace all the necessary files in this directory.
+* Take note of the files related to Laravel Mix. Keep them if you need.
+
+
+## Compiling Assets
+
+* This project make use of [Laravel Mix](https://laravel.com/docs/master/mix)
+* Asset will be compile into `dist`.
+* Versioning / Cache Busting with the `mix` function available with the helpers functions. Refer to [./lib/theme-helpers.php](./lib/theme-helpers.php)
---
+++ webpack.mix.js
@@ -0,0 +1,29 @@
+/*
+ |--------------------------------------------------------------------------
+ | Make use of Laravel Mix for compiling assets
+ |--------------------------------------------------------------------------
+ */
+
+const mix = require('laravel-mix');
+
+// Change Default Larvel Mix WebPack Config
+mix.setPublicPath('dist'); 
+mix.setResourceRoot('/wp-content/themes/' + path.relative('../', 'dist'));
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.js('static/site.js', 'dist/js');
+
+// Versioning only in Production
+if (mix.inProduction()) {
+    mix.version();
+}
